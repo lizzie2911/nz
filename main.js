6e4494173e5e21c797a36a8d069a04b9767c859a
Elisabeth Erdozain <lizzie.erdozain@gmail.com>
2025-03-27 15:59:21 +0100

    added coordinates in squiggley brackets in popup comment

--- main.js
+++ main.js
@@ -22,5 +22,5 @@ let marker = L.marker([lat, lon]).addTo(map);
 // Popup defined and opened
 marker.bindPopup(`
     <b>Hello world!</b>
-    <br>Welcome to Wanaka!! :)
+    <br>Welcome to Wanaka at ${lat} / ${lon} !! :)
 `).openPopup();
